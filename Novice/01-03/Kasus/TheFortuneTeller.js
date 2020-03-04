75201522b978fda1dc7588a9009f5fceb1f086fe
nasri1204 <55940240+nasri1204@users.noreply.github.com>
2020-03-04 14:08:58 +0700

    Delete TheFortuneTeller.js

--- Novice/01-03/Kasus/TheFortuneTeller.js
+++
@@ -1,8 +0,0 @@
-function tellFortune(jobTitle, locGeog, nameMitra, jumAnk){
-    var future = 'Anda akan menjadi ' + jobTitle + ' di ' + locGeog + ' dan menikah dengan ' + nameMitra + ' dengan ' + jumAnk + 'anak.';
-    console.log(future);
-}
-
-tellFortune('bball player', 'spain', 'Shaq', 3);
-tellFortune('stunt double', 'Japan', 'Ryan Gosling', 3000);
-tellFortune('Elvis impersonator', 'Russia', 'The Oatmeal', 0);
