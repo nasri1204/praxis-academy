de9d6f6c4c189be97ea19db5254e1be4ee0c483f
nasri1204 <55940240+nasri1204@users.noreply.github.com>
2020-03-04 14:09:06 +0700

    Delete TheGeometrizer.js

--- Novice/01-03/Kasus/TheGeometrizer.js
+++
@@ -1,9 +0,0 @@
-function calcGeometry(radius) {
-    var circumference = Math.PI * 2*radius;
-    console.log("The circumference is " + circumference);
-
-    var area = Math.PI * radius*radius;
-    console.log("The area is " + area);
-}
-
-calcGeometry(10)
