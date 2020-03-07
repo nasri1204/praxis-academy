4a01c08225d90d1caf57522f74f8c4cf58e8fc7e
nasri1204 <nasri.w1230@gmail.com>
2020-03-07 19:21:53 +0700

    hari ke lima

---
+++ Novice/01-05/Kasus/kasus1.js
@@ -0,0 +1,13 @@
+var data_a = [3, 2, 4];
+var data_b = [5, 1, 6];
+
+var hasil_1 = data_a[0] + data_b[0];
+var hasil_2 = data_a[1] + data_b[1];
+var hasil_3 = data_a[2] + data_b[2];
+
+
+console.log(data_a[0], data_a[1], data_a[2]);
+console.log(data_b[0], data_b[1], data_b[2]);
+console.log("Hasil :");
+
+console.log(hasil_1, hasil_2, hasil_3);
