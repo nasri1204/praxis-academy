1eb82a0323c857ee3a244c1cd2e68d20e97097d9
nasri1204 <55940240+nasri1204@users.noreply.github.com>
2020-03-12 19:33:01 +0700

    Delete readingFile.js

--- Novice/02-03/Latihan/readingFile.js
+++
@@ -1,6 +0,0 @@
-const charactersCounter = (text) => `Character count: ${text.length}`;
-
-function analyzeFile(filename) {
-    let fileContent = open(filename);
-    return charactersCounter(fileContent);
-}
